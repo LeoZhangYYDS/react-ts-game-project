bdf5b6aef3c0355b9517e0dc81c428ace3b3cb73
LeoZhangYYDS <zhanglingxiao1991@gmail.com>
2024-09-03 14:50:25 +1000

    Create a generic data fetch

---
+++ src/hooks/useData.ts
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
+const useData = <T>(endpoint: string) => {
+  const [data, setData] = useState<T[]>([]);
+  const [error, setError] = useState([""]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    //clean up 1
+    const controller = new AbortController();
+    //before call api setLoading to true
+    setLoading(true);
+    apiClient
+      .get<FetchResponse<T>>(
+        endpoint,
+        //clean up 2
+        { signal: controller.signal }
+      )
+      .then((res) => {
+        setData(res.data.results);
+        //called api ,get results, then setLoading to false
+        setLoading(false);
+      })
+      .catch((err) => {
+        //clean up 3
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        //called api ,get error, then setLoading to false
+        setLoading(false);
+      });
+    //clean up 4
+    return () => controller.abort();
+  }, []);
+
+  return { data, error, loading };
+};
+
+export default useData;
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import useData from "./useData";
 
 export interface Platform {
   id: number;
@@ -14,45 +12,8 @@ export interface Game {
   parent_platforms: { platform: Platform }[];
   metacritic: number;
 }
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
 
 //build a custom hook，so the whole app can use to fetch games
-const useGames = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState([""]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    //clean up 1
-    const controller = new AbortController();
-    //before call api setLoading to true
-    setLoading(true);
-    apiClient
-      .get<FetchGamesResponse>(
-        "/games",
-        //clean up 2
-        { signal: controller.signal }
-      )
-      .then((res) => {
-        setGames(res.data.results);
-        //called api ,get results, then setLoading to false
-        setLoading(false);
-      })
-      .catch((err) => {
-        //clean up 3
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        //called api ,get error, then setLoading to false
-        setLoading(false);
-      });
-    //clean up 4
-    return () => controller.abort();
-  }, []);
-
-  return { games, error, loading };
-};
+const useGames = () => useData<Game>("/games");
 
 export default useGames;
