4be03191cf03b6a4f0c0f3ac6ad3758b5fe5cbd8
LeoZhangYYDS <106665596+LeoZhangYYDS@users.noreply.github.com>
2024-09-17 15:08:07 +1000

    fix search error

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,14 +10,23 @@ interface Props {
 }
 
 const GameCard = ({ game }: Props) => {
+  // const limit = 17;
+
+  // const truncateText = (text: string, limit: number) => {
+  //   if (text.length > limit) {
+  //     return text.slice(0, limit) + "...";
+  //   }
+  //   return text;
+  // };
+
   return (
     <Link to={"/games/" + game.slug}>
-      <Card height="400">
+      <Card>
         <Image src={resizeImage(game.background_image)} />
         <CardBody>
           <HStack justifyContent="space-between" marginBottom={3}>
             <PlatformIconList
-              platforms={game.parent_platforms.map((p) => p.platform)}
+              platforms={game.parent_platforms?.map((p) => p.platform) || []}
             />
             <CriticScore score={game.metacritic} />
           </HStack>
